400c434fbc151e14bd287550bc15cd4e9b264d03
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-05-01 17:15:05 +0500

    hmm

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-export default function Home()  {
+export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Amazon Blockchain App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
