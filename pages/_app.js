e77b9ad5ab66540b8d57d60a1f2d66270b4a9d36
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-05-06 20:30:50 +0500

    bring back

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
+import { AmazonProvider } from "../context/AmazonContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId="hWKs6uoexB2oa1i2dyd39PpEMGPKcPv4n4QJtxIN"
+      serverUrl="https://t1zjikvshoiq.usemoralis.com:2053/server"
+    >
+      <AmazonProvider>
+        <Component {...pageProps} />
+      </AmazonProvider>
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
