b9fea1d632ac9ab1f75ae30e74f22c0e9aebacb1
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-05-12 22:39:57 +0500

    hmm

--- next.config.js
+++ next.config.js
@@ -3,17 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      "lh3.googleusercontent.com",
-      "openseauserdata.com",
-      "brand.assets.adidas.com",
-      "media0.giphy.com",
-      "avatars.dicebear.com",
-      "media1.giphy.com",
-      "media3.giphy.com",
-      "media2.giphy.com",
-      "media4.giphy.com",
+      'lh3.googleusercontent.com',
+      'openseauserdata.com',
+      'brand.assets.adidas.com',
+      'media0.giphy.com',
+      'avatars.dicebear.com',
+      'media1.giphy.com',
+      'media3.giphy.com',
+      'media2.giphy.com',
+      'media4.giphy.com',
     ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
